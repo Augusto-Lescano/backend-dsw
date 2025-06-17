b25ea6f4cf2d468666b044998dd79db18f679777
Gian Marco <mercanzinigianmarco@gmail.com>
2025-06-17 12:46:01 -0300

    Creacion crud torneo

---
+++ dist/torneo/torneo.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { sanitizeTorneoInput, findAll, findOne, add, update, remove } from "./torneo.controller.js";
+export const torneoRouter = Router();
+torneoRouter.get('/', findAll);
+torneoRouter.get('/:id', findOne);
+torneoRouter.post('/', sanitizeTorneoInput, add);
+torneoRouter.put('/:id', sanitizeTorneoInput, update);
+torneoRouter.patch('/:id', sanitizeTorneoInput, update);
+torneoRouter.delete('/:id', remove);
+//# sourceMappingURL=torneo.routes.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import { torneoRouter } from './torneo/torneo.routes.js'
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
+app.use('/api/torneos', torneoRouter)
 
 app.use((req, res) => {
     res.status(404).send({message: 'Resource not found'})
