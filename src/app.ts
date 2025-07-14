e12f9bb67a1ad7f80da88ab030bdd664a7ca37db
Augusto Lescano <164239807+Augusto-Lescano@users.noreply.github.com>
2025-07-14 16:26:49 -0300

    feature/creacion-CRUD-tipoDeTorneo (#7)

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
-import express, {NextFunction, Request, Response} from 'express'
+import express from 'express'
 import { torneoRouter } from './torneo/torneo.routes.js'
 import { usuarioRouter } from './usuario/usuario.routes.js'
+import { tipoDeTorneoRouter } from './tipoDeTorneo/tipoDeTorneo.routes.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/torneos', torneoRouter)
 app.use('/api/usuarios', usuarioRouter)
+app.use('/api/tipoDeTorneo',tipoDeTorneoRouter)
 
 app.use((req,res)=>{
     res.status(404).send({message:'Resource not found'})
