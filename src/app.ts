c6b598d7cf91a1d2a1eb055e5cbcce6be5d3b969
Jose Emilio Certo <emiliokmkz@gmail.com>
2025-08-07 21:21:00 -0300

    pasar a mikro-orm crud usuario

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,60 @@
-import express from 'express'
-import { torneoRouter } from './torneo/torneo.routes.js'
-import { usuarioRouter } from './usuario/usuario.routes.js'
-import { tipoDeTorneoRouter } from './tipoDeTorneo/tipoDeTorneo.routes.js';
-import { tipoDeJuegoRouter } from './tipoDeJuego/tipoDeJuego.routes.js';
+import 'reflect-metadata' 
 
-const app = express();
-app.use(express.json());
+import express from 'express' 
 
-app.use('/api/torneos', torneoRouter)
-app.use('/api/usuarios', usuarioRouter)
-app.use('/api/tipoDeTorneo',tipoDeTorneoRouter)
-app.use("/api/tipoDeJuego",tipoDeJuegoRouter)
+import { torneoRouter } from './torneo/torneo.routes.js' 
+import { usuarioRouter } from './usuario/usuario.routes.js' 
 
-app.use((req,res)=>{
-    res.status(404).send({message:'Resource not found'})
-})
+import { tipoDeTorneoRouter } from './tipoDeTorneo/tipoDeTorneo.routes.js'; 
 
-app.listen(3000,()=>{
-    console.log("Server running on http://localhost:3000/")
-})
+import { tipoDeJuegoRouter } from './tipoDeJuego/tipoDeJuego.routes.js'; 
+
+import { orm, syncSchema } from './shared/db/orm.js';
+
+import { RequestContext } from '@mikro-orm/core' 
+
+ 
+
+const app = express(); 
+
+app.use(express.json()); 
+
+ 
+
+app.use((req, res, next) => { 
+
+  RequestContext.create(orm.em, next) 
+
+}) 
+
+ 
+
+app.use('/api/torneos', torneoRouter) 
+
+app.use('/api/usuarios', usuarioRouter) 
+
+app.use('/api/tipoDeTorneo',tipoDeTorneoRouter) 
+
+app.use("/api/tipoDeJuego",tipoDeJuegoRouter) 
+
+ 
+
+app.use((req,res)=>{ 
+
+    res.status(404).send({message:'Resource not found'}) 
+
+}) 
+
+ 
+
+await syncSchema()  
+
+ 
+
+app.listen(3000,()=>{ 
+
+    console.log("Server running on http://localhost:3000/") 
+
+}) 
+
+ 
