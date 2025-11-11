9382d454437598d40a2acac0494cc0920950cacd
Gian Marco <mercanzinigianmarco@gmail.com>
2025-11-11 20:49:52 -0300

    se eliminan archivos innecesarios

--- src/inscripcion/inscripcion.entity.ts
+++ src/inscripcion/inscripcion.entity.ts
@@ -1,31 +1,29 @@
-import { Entity, Property, OneToMany,OneToOne, Rel,Collection,Cascade} from "@mikro-orm/core";
+import { Entity, Property, OneToMany, OneToOne, Rel, Collection } from "@mikro-orm/core";
 import { BaseEntity } from "../shared/db/baseEntity.entity.js";
-import { Equipo } from "../equipo/equipo.entity.js";
 import { Torneo } from "../torneo/torneo.entity.js";
 import { InscripcionIndividual } from "./inscripcionIndividual.entity.js";
+import { InscripcionEquipo } from "./inscripcionEquipo.entity.js";
 
 @Entity()
 export class Inscripcion extends BaseEntity {
 
-  @Property({nullable: false,default: 'abierta'})
+  @Property({ nullable: false, default: 'abierta' })
   estado!: string;
 
-  @Property({nullable: false, default: '2024-01-01T00:00:00' })
+  @Property({ nullable: false })
   fechaApertura!: Date;
 
-  @Property({nullable: false, default: '2024-12-31T23:59:59' })
+  @Property({ nullable: false })
   fechaCierre!: Date;
 
-  @OneToMany(() => Equipo, equipo => equipo.inscripcion, {
-        cascade: [Cascade.ALL]
-    })
-    equipos = new Collection<Equipo>(this);
+  @OneToMany(() => InscripcionIndividual, inscIndividual => inscIndividual.inscripcion)
+  inscripcionesIndividuales = new Collection<InscripcionIndividual>(this);
 
-    @OneToMany(() => InscripcionIndividual, inscIndividual => inscIndividual.inscripcion)
-    inscripcionesIndividuales = new Collection<InscripcionIndividual>(this);
+  @OneToMany(() => InscripcionEquipo, inscEquipo => inscEquipo.inscripcion)
+  inscripcionesEquipos = new Collection<InscripcionEquipo>(this);
 
   @OneToOne(() => Torneo, torneo => torneo.inscripcion, { 
-        nullable: false 
-    })
-    torneo!: Rel<Torneo>;
+    nullable: false 
+  })
+  torneo!: Rel<Torneo>;
 }
--- src/service/inscripcion/estadoInscripcion.ts
+++
@@ -1,9 +0,0 @@
-
-function calcularEstadoInscripcion(fechaInicioIns: Date, fechaFinIns: Date): string{
-    const ahora = new Date();
-    if (ahora < fechaInicioIns) return 'Proximamente';
-    if (ahora <= fechaFinIns) return 'Abierta';
-    return 'Cerrada';
-}
-
-export {calcularEstadoInscripcion}
