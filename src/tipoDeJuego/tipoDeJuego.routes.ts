1fbae313b9d75292c14001d4e0026120c5769e67
Gian Marco <mercanzinigianmarco@gmail.com>
2025-11-12 11:08:39 -0300

    tipoDejuego metodos http actualizados

--- src/tipoDeJuego/tipoDeJuego.routes.ts
+++ src/tipoDeJuego/tipoDeJuego.routes.ts
@@ -17,11 +17,3 @@ tipoDeJuegoRouter.get("/:id", requireAuth, requireAdmin, findOne)
 tipoDeJuegoRouter.post("/", requireAuth, requireAdmin, sanitizedTipoDeJuegoInput, add)
 tipoDeJuegoRouter.put("/:id", requireAuth, requireAdmin, sanitizedTipoDeJuegoInput, update)
 tipoDeJuegoRouter.delete("/:id", requireAuth, requireAdmin, remove)
-
-/*
-GET    /api/tipoDeJuego           # Listar tipos de juego (solo admin)
-GET    /api/tipoDeJuego/1         # Ver tipo específico (solo admin)  
-POST   /api/tipoDeJuego           # Crear tipo (solo admin)
-PUT    /api/tipoDeJuego/1         # Actualizar tipo (solo admin)
-DELETE /api/tipoDeJuego/1         # Eliminar tipo (solo admin)
-*/
